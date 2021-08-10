dea12fc622e3a9ae4bb8adb8fa8943920887d856
sandeep-kodari <88716625+sandeep-kodari@users.noreply.github.com>
2021-08-10 16:47:44 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st 
+import pytesseract 
+from PIL import Image 
+st.title("OCR-optical character recognition") 
+img=st.file_uploader("choose an image") 
+if image is not None: 
+  img_read=Image.open(img) 
+  st.image(img,caption='uploaded image') 
+  if st.button('predict'): 
+    op=pytesseract.image_to_string(img_read) 
+    st.write(op)
